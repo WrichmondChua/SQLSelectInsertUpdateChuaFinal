ee6c71bed3ad800cb3ab788f52176e4ec1a308f8
Wrichmond Well A. Chua <59139222+WrichmondChua@users.noreply.github.com>
2023-02-04 21:38:18 +0800

    Add files via upload

---
+++ SQLSelectInsertUpdateChuaFinal/SQLSelectInsertUpdateChuaFinal/FrmClubRegistration.Designer.cs
@@ -0,0 +1,321 @@
+﻿
+namespace SQLSelectInsertUpdateChuaFinal
+{
+    partial class FrmClubRegistration
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxStudentID = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxLastName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxFirstName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxMiddleName = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBoxProgram = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxAge = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.comboBoxGender = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.buttonRegister = new System.Windows.Forms.Button();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.buttonRefresh = new System.Windows.Forms.Button();
+            this.dataGridViewListOfClubMembers = new System.Windows.Forms.DataGridView();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBoxID = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewListOfClubMembers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(250, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Student ID:";
+            // 
+            // textBoxStudentID
+            // 
+            this.textBoxStudentID.Location = new System.Drawing.Point(254, 43);
+            this.textBoxStudentID.Name = "textBoxStudentID";
+            this.textBoxStudentID.Size = new System.Drawing.Size(231, 23);
+            this.textBoxStudentID.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(6, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 21);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Last Name:";
+            // 
+            // textBoxLastName
+            // 
+            this.textBoxLastName.Location = new System.Drawing.Point(8, 109);
+            this.textBoxLastName.Name = "textBoxLastName";
+            this.textBoxLastName.Size = new System.Drawing.Size(231, 23);
+            this.textBoxLastName.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(250, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 21);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "First Name:";
+            // 
+            // textBoxFirstName
+            // 
+            this.textBoxFirstName.Location = new System.Drawing.Point(254, 109);
+            this.textBoxFirstName.Name = "textBoxFirstName";
+            this.textBoxFirstName.Size = new System.Drawing.Size(231, 23);
+            this.textBoxFirstName.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(495, 85);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(107, 21);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Middle Name:";
+            // 
+            // textBoxMiddleName
+            // 
+            this.textBoxMiddleName.Location = new System.Drawing.Point(495, 109);
+            this.textBoxMiddleName.Name = "textBoxMiddleName";
+            this.textBoxMiddleName.Size = new System.Drawing.Size(231, 23);
+            this.textBoxMiddleName.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(494, 14);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(74, 21);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Program:";
+            // 
+            // comboBoxProgram
+            // 
+            this.comboBoxProgram.FormattingEnabled = true;
+            this.comboBoxProgram.Location = new System.Drawing.Point(513, 56);
+            this.comboBoxProgram.Name = "comboBoxProgram";
+            this.comboBoxProgram.Size = new System.Drawing.Size(294, 23);
+            this.comboBoxProgram.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(6, 153);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(40, 21);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Age:";
+            // 
+            // textBoxAge
+            // 
+            this.textBoxAge.Location = new System.Drawing.Point(7, 177);
+            this.textBoxAge.Name = "textBoxAge";
+            this.textBoxAge.Size = new System.Drawing.Size(63, 23);
+            this.textBoxAge.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(250, 153);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(64, 21);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Gender:";
+            // 
+            // comboBoxGender
+            // 
+            this.comboBoxGender.FormattingEnabled = true;
+            this.comboBoxGender.Location = new System.Drawing.Point(255, 177);
+            this.comboBoxGender.Name = "comboBoxGender";
+            this.comboBoxGender.Size = new System.Drawing.Size(141, 23);
+            this.comboBoxGender.TabIndex = 13;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBoxID);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.comboBoxGender);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.textBoxAge);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.textBoxMiddleName);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.textBoxFirstName);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBoxLastName);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBoxStudentID);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(7, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(806, 261);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            // 
+            // buttonRegister
+            // 
+            this.buttonRegister.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonRegister.Location = new System.Drawing.Point(819, 26);
+            this.buttonRegister.Name = "buttonRegister";
+            this.buttonRegister.Size = new System.Drawing.Size(106, 47);
+            this.buttonRegister.TabIndex = 15;
+            this.buttonRegister.Text = "Register";
+            this.buttonRegister.UseVisualStyleBackColor = true;
+            this.buttonRegister.Click += new System.EventHandler(this.buttonRegister_Click);
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonUpdate.Location = new System.Drawing.Point(819, 97);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(106, 47);
+            this.buttonUpdate.TabIndex = 16;
+            this.buttonUpdate.Text = "Update";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.buttonRefresh);
+            this.groupBox2.Controls.Add(this.dataGridViewListOfClubMembers);
+            this.groupBox2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.groupBox2.Location = new System.Drawing.Point(13, 284);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(840, 236);
+            this.groupBox2.TabIndex = 17;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "List Of Club Members";
+            // 
+            // buttonRefresh
+            // 
+            this.buttonRefresh.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonRefresh.Location = new System.Drawing.Point(715, 28);
+            this.buttonRefresh.Name = "buttonRefresh";
+            this.buttonRefresh.Size = new System.Drawing.Size(119, 56);
+            this.buttonRefresh.TabIndex = 18;
+            this.buttonRefresh.Text = "Refresh";
+            this.buttonRefresh.UseVisualStyleBackColor = true;
+            this.buttonRefresh.Click += new System.EventHandler(this.buttonRefresh_Click);
+            // 
+            // dataGridViewListOfClubMembers
+            // 
+            this.dataGridViewListOfClubMembers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewListOfClubMembers.Location = new System.Drawing.Point(8, 21);
+            this.dataGridViewListOfClubMembers.Name = "dataGridViewListOfClubMembers";
+            this.dataGridViewListOfClubMembers.RowTemplate.Height = 25;
+            this.dataGridViewListOfClubMembers.Size = new System.Drawing.Size(701, 214);
+            this.dataGridViewListOfClubMembers.TabIndex = 0;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label8.Location = new System.Drawing.Point(6, 22);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(28, 21);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "ID:";
+            // 
+            // textBoxID
+            // 
+            this.textBoxID.Location = new System.Drawing.Point(5, 44);
+            this.textBoxID.Name = "textBoxID";
+            this.textBoxID.Size = new System.Drawing.Size(231, 23);
+            this.textBoxID.TabIndex = 15;
+            // 
+            // FrmClubRegistration
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(928, 534);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.buttonRegister);
+            this.Controls.Add(this.comboBoxProgram);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "FrmClubRegistration";
+            this.Text = "FrmClubRegistration";
+            this.Load += new System.EventHandler(this.FrmClubRegistration_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewListOfClubMembers)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxStudentID;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxLastName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxFirstName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxMiddleName;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBoxProgram;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxAge;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox comboBoxGender;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button buttonRegister;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button buttonRefresh;
+        private System.Windows.Forms.DataGridView dataGridViewListOfClubMembers;
+        private System.Windows.Forms.TextBox textBoxID;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ SQLSelectInsertUpdateChuaFinal/SQLSelectInsertUpdateChuaFinal/FrmClubRegistration.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace SQLSelectInsertUpdateChuaFinal
+{
+    public partial class FrmClubRegistration : Form
+    {
+        public FrmClubRegistration()
+        {
+            InitializeComponent();
+            comboBoxGender.Items.Add("Male");
+            comboBoxGender.Items.Add("Female");
+            comboBoxProgram.Items.Add("BS in Information Management");
+        }
+
+        private void FrmClubRegistration_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonRefresh_Click(object sender, EventArgs e)
+        {
+            SqlConnection sqlConnection = new SqlConnection("Data Source = (LocalDB)\\MSSQLLocalDB; AttachDbFilename = C:\\Users\\ADMIN\\source\\repos\\SQLSelectInsertUpdateChuaFinal\\SQLSelectInsertUpdateChuaFinal\\Database1.mdf; Integrated Security = True");
+            sqlConnection.Open();
+            SqlCommand sqlCommand = new SqlCommand("Select * from ClubMembers", sqlConnection);
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+            DataTable dataTable = new DataTable();
+            sqlDataAdapter.Fill(dataTable);
+            dataGridViewListOfClubMembers.DataSource = dataTable;
+        }
+
+        private void buttonRegister_Click(object sender, EventArgs e)
+        {
+            SqlConnection sqlConnection = new SqlConnection("Data Source = (LocalDB)\\MSSQLLocalDB; AttachDbFilename = C:\\Users\\ADMIN\\source\\repos\\SQLSelectInsertUpdateChuaFinal\\SQLSelectInsertUpdateChuaFinal\\Database1.mdf; Integrated Security = True");
+            sqlConnection.Open();
+            SqlCommand sqlCommand = new SqlCommand("insert into ClubMembers values (@ID, @StudentId, @FirstName,@MiddleName, @LastName, " +
+                "@Age, @Gender, @Program where ID= @ID)", sqlConnection);
+
+            sqlCommand = new SqlCommand("INSERT INTO ClubMembers VALUES (@ID, @StudentID, @FirstName, " +
+                "@MiddleName, @LastName, @Age, @Gender, @Program)", sqlConnection);
+
+            sqlCommand.Parameters.Add("@ID", SqlDbType.Int).Value = int.Parse(textBoxID.Text);
+            sqlCommand.Parameters.Add("@StudentID", SqlDbType.Int).Value = int.Parse(textBoxStudentID.Text);
+            sqlCommand.Parameters.Add("@FirstName", SqlDbType.VarChar).Value = textBoxFirstName.Text;
+            sqlCommand.Parameters.Add("@MiddleName", SqlDbType.VarChar).Value= textBoxMiddleName.Text;
+            sqlCommand.Parameters.Add("@LastName", SqlDbType.VarChar).Value = textBoxLastName.Text;
+            sqlCommand.Parameters.Add("@Age", SqlDbType.Int).Value = int.Parse(textBoxAge.Text);
+            sqlCommand.Parameters.Add("@Gender", SqlDbType.VarChar).Value = comboBoxGender.Text;
+            sqlCommand.Parameters.Add("@Program", SqlDbType.VarChar).Value = comboBoxProgram.Text;
+
+            if (sqlCommand.Connection.State==ConnectionState.Open)
+            {
+                sqlCommand.Connection.Close();
+            }
+            sqlConnection.Open();
+            sqlCommand.ExecuteNonQuery();
+            sqlConnection.Close();
+
+            MessageBox.Show("Successfully Registered.");
+
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            SqlConnection sqlConnection = new SqlConnection("Data Source = (LocalDB)\\MSSQLLocalDB; AttachDbFilename = C:\\Users\\ADMIN\\source\\repos\\SQLSelectInsertUpdateChuaFinal\\SQLSelectInsertUpdateChuaFinal\\Database1.mdf; Integrated Security = True");
+            sqlConnection.Open();
+            SqlCommand sqlCommand = new SqlCommand("insert into ClubMembers values (@ID, @StudentId, @FirstName,@MiddleName, @LastName, " +
+                "@Age, @Gender, @Program where ID= @ID)", sqlConnection);
+
+            sqlCommand = new SqlCommand("INSERT INTO ClubMembers VALUES (@ID, @StudentID, @FirstName, " +
+                "@MiddleName, @LastName, @Age, @Gender, @Program)", sqlConnection);
+
+            sqlCommand.Parameters.Add("@ID", SqlDbType.Int).Value = int.Parse(textBoxID.Text);
+            sqlCommand.Parameters.Add("@StudentID", SqlDbType.Int).Value = int.Parse(textBoxStudentID.Text);
+            sqlCommand.Parameters.Add("@FirstName", SqlDbType.VarChar).Value = textBoxFirstName.Text;
+            sqlCommand.Parameters.Add("@MiddleName", SqlDbType.VarChar).Value = textBoxMiddleName.Text;
+            sqlCommand.Parameters.Add("@LastName", SqlDbType.VarChar).Value = textBoxLastName.Text;
+            sqlCommand.Parameters.Add("@Age", SqlDbType.Int).Value = int.Parse(textBoxAge.Text);
+            sqlCommand.Parameters.Add("@Gender", SqlDbType.VarChar).Value = comboBoxGender.Text;
+            sqlCommand.Parameters.Add("@Program", SqlDbType.VarChar).Value = comboBoxProgram.Text;
+
+            if (sqlCommand.Connection.State == ConnectionState.Open)
+            {
+                sqlCommand.Connection.Close();
+            }
+            sqlConnection.Open();
+            sqlCommand.ExecuteNonQuery();
+            sqlConnection.Close();
+
+            MessageBox.Show("Successfully Updated.");
+
+        }
+    }
+    }
+
